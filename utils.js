7276b17753a4d6d5728157e7ecfc2258229aba51
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:01:13 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:01:01.462Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T13:01:10.867Z
+Commit: Fix broken links in documentation
