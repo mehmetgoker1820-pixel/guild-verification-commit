6fc66187639da0096ac805e450bf2ee8caff9414
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:54:57 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:53:45.160Z
-Commit: Fix database connection timeout
+Timestamp: 2026-04-29T12:54:55.016Z
+Commit: Fix CSS styling bug on mobile devices
