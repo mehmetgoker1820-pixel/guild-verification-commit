c26979a66b5600a4eb2bf2b4a94a87abc9eabfee
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:54:24 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:54:02.061Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-29T12:54:22.074Z
+Commit: Fix CSS styling bug on mobile devices
