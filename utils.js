fd2839109a3e9a36976c0538342682a4ca42d655
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:02:15 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:02:10.451Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T13:02:13.060Z
+Commit: Resolve timezone issues in date handling
