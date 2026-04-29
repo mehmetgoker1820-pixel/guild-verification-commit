a9ddcf53c7ae011ca765fa8c2c2f315b8562c3c0
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:01:38 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:01:20.411Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T13:01:35.531Z
+Commit: Fix broken links in documentation
