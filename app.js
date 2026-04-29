43cd6ff31cf43de93f80dc8aaaaf29a936b16715
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:01:22 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:00:59.364Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-29T13:01:20.411Z
+Commit: Fix CSS styling bug on mobile devices
