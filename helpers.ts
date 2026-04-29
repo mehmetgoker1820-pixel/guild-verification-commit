028874f12c6ae4059b370ddd1e8c5f432b78b3fe
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:00:05 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:59:21.533Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-29T13:00:03.454Z
+Commit: Introduce real-time notifications
