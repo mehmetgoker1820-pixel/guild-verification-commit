38a71737816b1c29f52d8c7dce53a55cb332f650
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:09:27 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:06:51.322Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-29T12:09:24.638Z
+Commit: Fix broken links in documentation
