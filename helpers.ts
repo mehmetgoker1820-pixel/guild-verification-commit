d9021ef48636a3342071f2e26c62e5a2587ebe2e
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:55:06 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:55:01.653Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-29T12:55:03.997Z
+Commit: Add dark mode support
