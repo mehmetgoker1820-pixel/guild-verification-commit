eb5292c34b4115987b70741c591b8137d036f81a
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:01:36 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:01:24.123Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-29T13:01:33.571Z
+Commit: Introduce real-time notifications
