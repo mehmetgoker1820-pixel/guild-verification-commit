f7180dc02d9903c1347ea016072040aef44bbdee
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:59:23 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:59:17.887Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-29T12:59:21.533Z
+Commit: Resolve null pointer exception in user service
