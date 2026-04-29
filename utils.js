16faf737d1af76b2ade0f4ffe2d610dea8d574a2
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:53:53 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:07:47.493Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-04-29T12:53:50.881Z
+Commit: Add export functionality for reports
