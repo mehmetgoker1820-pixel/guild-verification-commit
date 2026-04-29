d21beb21c722ac506e016da3448892bd1731c1ce
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:48:00 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:09:24.638Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-29T12:47:58.394Z
+Commit: Patch API endpoint error handling
