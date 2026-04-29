c19b15989ad2b7e591af95fde4fc8b2712ff4dbf
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:02:42 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:02:13.060Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-29T13:02:39.638Z
+Commit: Fix broken links in documentation
