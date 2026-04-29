ccc6285ae799ed9c54d304e3c11fe80cf4fd74b9
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:59:20 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:59:03.121Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-29T12:59:17.887Z
+Commit: Patch API endpoint error handling
