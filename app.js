07c3015fc5f8d671a332d95f831fe58850a833cc
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:01:45 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:01:37.434Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-29T13:01:42.784Z
+Commit: Fix broken links in documentation
