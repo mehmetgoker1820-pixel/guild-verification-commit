cd23d85052c7a601bd40bb8811951428d2d78337
Bar <mehmetgoker1820@gmail.com>
2026-04-29 16:03:20 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T13:02:39.638Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-29T13:03:18.176Z
+Commit: Resolve timezone issues in date handling
