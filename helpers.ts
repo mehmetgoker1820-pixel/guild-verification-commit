c06c7d69d9ef536b6d923e291ece79a41b1a55a1
Bar <mehmetgoker1820@gmail.com>
2026-04-29 15:55:04 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T12:54:55.016Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T12:55:01.653Z
+Commit: Resolve null pointer exception in user service
